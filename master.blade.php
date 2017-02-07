afc318bf7f0cf3016488de0cf3ee8e9f0b37f39e
PaladinDigital <admin@paladindigital.co.uk>
2017-02-07 08:59:41 +0000

    Add foundation from cdn.

--- master.blade.php
+++ master.blade.php
@@ -8,7 +8,8 @@ $theme = 'themes::paladindigital.laravel-foundation';
 ?><!DOCTYPE html>
 <html>
 <head>
-
+    <!-- Foundation 6.2.4 CSS -->
+    <link rel="stylesheet" crossorigin="anonymous" integrity="sha256-NqI559iHqiWe5VxDi9mXE8i83ghn8Ru8BINyiPN7lDg=" href="https://cdn.jsdelivr.net/foundation/6.2.4/foundation.min.css">
 @yield('head')
 @yield('meta')
 @yield('styles')
@@ -21,7 +22,8 @@ $theme = 'themes::paladindigital.laravel-foundation';
 @yield('widgets')
 </aside>
 @include($theme . '_footer')
-
+    <!-- Foundation 6.2.4 JS -->
+    <script crossorigin="anonymous" integrity="sha256-6ikyM5zgWkLwRwc64Pn/i5ioDIvcxCtb+EXE8Zm0qK8=" src="https://cdn.jsdelivr.net/foundation/6.2.4/foundation.min.js"></script>
 @yield('scripts')
 </body>
 </html>
